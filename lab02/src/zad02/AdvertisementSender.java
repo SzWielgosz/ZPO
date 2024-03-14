56c765bf151af8e9d39ba020b6b50773f1d2e006
SzWielgosz <77066615+SzWielgosz@users.noreply.github.com>
2024-03-14 19:58:03 +0100

    fix

--- lab02/src/zad02/AdvertisementSender.java
+++ lab02/src/zad02/AdvertisementSender.java
@@ -3,21 +3,13 @@ package zad02;
 public class AdvertisementSender {
     SendMethod sendMethod;
 
-    Translation translation;
-
 
     public AdvertisementSender(SendMethod sendMethod) {
         this.sendMethod = sendMethod;
-        this.translation = new PolishTranslation();
-    }
-
-    public void checkTranslation(User user){
-        this.translation = user.getTranslation();
     }
 
     void doSendMessage(User user, Advertisement advertisement){
-        this.checkTranslation(user);
-        this.translation.translate();
+        user.getTranslation().translate();
         this.sendMethod.send(advertisement, user);
     }
 
